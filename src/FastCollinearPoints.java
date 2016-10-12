25ad771312b69ec056ea02bc3ff9abb09f49eea2
Emanuel <manu.grinberg16@gmail.com>
2016-10-12 18:35:49 +0300

    Some work

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -2,7 +2,9 @@
  * Created by wolf on 09/10/16.
  */
 public class FastCollinearPoints {
-    public FastCollinearPoints(Point[] points){}
+    public FastCollinearPoints(Point[] points) {
+    }
+
     public int numberOfSegments()        // the number of line segments
 
     public LineSegment[] segments()                // the line segments
